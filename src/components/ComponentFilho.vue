2356201854effec9790f80d1ef192aefe08c9a60
Rafael Poveda <raffael.poveda@gmail.com>
2023-05-11 07:44:44 -0300

    inital commit

---
+++ src/components/ComponentFilho.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>{{ texto }}</h1>
+        <img v-if="showImage" src="https://www.codemotion.com/magazine/wp-content/uploads/2022/08/VueJS-e1659695444607.jpg" style="witdh:250px; height:250px" />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        texto: {
+            type: String,
+            default: () => null
+        },
+        showImage: {
+            type: Boolean,
+            default: () => true
+        }
+    }    
+}
+</script>
+
+<style>
+h1 {
+    color: red;
+}
+</style>
---
+++ src/components/ComponentPai.vue
@@ -0,0 +1,33 @@
+<template>
+    <div v-html="components" />
+</template>
+
+<script>
+import ComponentFilho from './ComponentFilho.vue';
+import Vue from 'vue';
+export default {
+  data() {
+    return {
+      components: ''
+    }
+  },
+  mounted() {
+    for (let index = 0; index < 5; index++) {
+      const _class = Vue.extend(ComponentFilho);
+      const _component = new _class({
+        propsData: {
+          texto: `Ola mundo ${index};`,
+          showImage: true
+        }
+      })
+      _component.$mount()
+      this.components += _component.$el.innerHTML + "<br />"
+      _component.$destroy()
+    }
+    console.log('components', this.components)
+  }
+}
+</script>
+
+<style>
+</style>
